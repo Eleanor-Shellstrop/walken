eca3afd6b42d06b9a06444b0d15565b79a3570ca
Anne Ensign <anne.ensign@gmail.com>
2021-10-14 15:32:25 -0400

    Add web scraper and make csv of all cowbell songs.

--- cowbell.py
+++ cowbell.py
@@ -1,14 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import csv
 
 
 
-URL = 'http://ultimatecowbell.com/songview.php?idsongperm=4247'
-page = requests.get(URL)
+songs = []
 
 
-soup = BeautifulSoup(page.content, "html.parser")
+def main(start, end):
+	id = 1
+	while id >= start and id <= end:
+		user_agent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.37"
+		URL = f"http://ultimatecowbell.com/songview.php?idsongperm={id}"
+		response = requests.get(URL, headers={'User-Agent': user_agent})
+		html = response.content
+		soup = BeautifulSoup(html, "lxml")
+		song = []
+		for b in soup.find_all('b'):
+			if len(soup.find_all('b')) == 5:
+				song.append(b.get_text(strip=True))
+		songs.append(song)
+		id = id + 1
+	# Add data scraped into a dataframe 
+	df = pd.DataFrame(songs, columns = ['Band', 'Song', 'Album', 'Record_Company', 'Year']) 
+	df.to_csv(f'cowbell{start}_{end}.csv', sep=',', encoding='utf-8-sig', index = False)
 
-data = soup.find_all('b')
 
-print(data)
+main(1, 4247)
